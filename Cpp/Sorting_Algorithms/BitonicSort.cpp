da5e8c959980ebce28f49b8489a0f733f578aeaa
RaunakAgarwal18 <103554673+RaunakAgarwal18@users.noreply.github.com>
2022-10-28 14:18:06 +0530

    Create BitonicSort.cpp

---
+++ Cpp/Sorting_Algorithms/BitonicSort.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+void compAndSwap(int a[], int i, int j, int dir)
+{
+    if (dir==(a[i]>a[j]))
+        swap(a[i],a[j]);
+}
+void bitonicMerge(int a[], int low, int cnt, int dir)
+{
+    if (cnt>1)
+    {
+        int k = cnt/2;
+        for (int i=low; i<low+k; i++)
+            compAndSwap(a, i, i+k, dir);
+        bitonicMerge(a, low, k, dir);
+        bitonicMerge(a, low+k, k, dir);
+    }
+}
+void bitonicSort(int a[],int low, int cnt, int dir)
+{
+    if (cnt>1)
+    {
+        int k = cnt/2;
+        bitonicSort(a, low, k, 1);
+        bitonicSort(a, low+k, k, 0);
+        bitonicMerge(a,low, cnt, dir);
+    }
+}
+void sort(int a[], int N, int up)
+{
+    bitonicSort(a,0, N, up);
+}
+int main()
+{
+    int a[]= {5,3,7,1,2,8,5,2,9,6};
+    int N = sizeof(a)/sizeof(a[0]);
+ 
+    int up = 1; 
+    sort(a, N, up);
+ 
+    printf("Sorted array: \n");
+    for (int i=0; i<N; i++)
+        printf("%d ", a[i]);
+    return 0;
+}
